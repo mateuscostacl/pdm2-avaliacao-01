63e6ff23b4d98401cce2793276307a19c4efee2a
Mateus Costa <mm1071259@gmail.com>
2022-04-08 23:55:34 -0300

    (Q4=ok) Adicionado método maiorValorNoArray para classe Util

--- app/src/main/java/com/example/pdm2_avaliacao_01/Util.java
+++ app/src/main/java/com/example/pdm2_avaliacao_01/Util.java
@@ -22,6 +22,23 @@ public class Util {
         return gerador.nextInt(max + 1 - min) + min;
     }
 
+    /**
+     *
+     * @param arrayDeNumeros Array de numeros inteiros
+     * @return Retorna a posicao (indice) do maior valor dentro do array
+     */
+    public static int maiorValorNoArray(int[] arrayDeNumeros){
+        int maior = Integer.MIN_VALUE;
+        int indiceMaior = -1;
+        for (int i = 0; i < arrayDeNumeros.length; i++) {
+            if (arrayDeNumeros[i] > maior) {
+                maior = arrayDeNumeros[i];
+                indiceMaior = i;
+            }
+        }
+        return indiceMaior;
+    }
+
     public static void mostrarUmaMensagem_Snackbar(View view, String mensagem) {
         Snackbar.make(view, mensagem, Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show();
